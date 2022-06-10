3f3cc43d5f10840cee83066db855b5e077ae4a9e
Lucre <josielindamaldonado@gmail.com>
2022-06-10 08:33:31 -0400

    fetched data for geocoding and current weather.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,31 @@
-console.log("world")
+var apiKey = '2524e9bb2b5f261c8205802844842a68';
+
+// FUNCTION CALLS ---------------------------------------------------------------------------
+var getGeocoding = function () {
+    var city = 'atlanta';
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    
+    fetch(apiUrl).then(function (response){
+        if (response.ok) {
+            response.json().then(function (data){
+                var lon = data.coord.lon;
+                var lat = data.coord.lat;
+                // console.log(data);
+                getCurrentWeather(lon, lat);
+            })
+        }
+    })
+};
+getGeocoding();
+
+var getCurrentWeather = function (lon, lat) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey
+
+    fetch(apiUrl).then(function (response){
+        if (response.ok) {
+            response.json().then(function (data){
+                console.log(data);
+            })
+        }
+    })
+}
