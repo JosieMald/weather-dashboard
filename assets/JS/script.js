08d4c81ece10caff06b0f7aff927f3253c07e7de
Lucre <josielindamaldonado@gmail.com>
2022-06-10 12:03:09 -0400

    fecthed 5 day forecast api.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,8 +2,8 @@ var apiKey = '2524e9bb2b5f261c8205802844842a68';
 
 // FUNCTION CALLS ---------------------------------------------------------------------------
 var getGeocoding = function () {
-    var city = 'atlanta';
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    var zipcode = '30052';
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=" + apiKey;
     
     fetch(apiUrl).then(function (response){
         if (response.ok) {
@@ -24,7 +24,30 @@ var getCurrentWeather = function (lon, lat) {
     fetch(apiUrl).then(function (response){
         if (response.ok) {
             response.json().then(function (data){
+                // console.log(data);
+                // console.log(data.name);
+                // console.log(Math.floor(data.main.temp));
+                // console.log(data.weather[0].main)
+                getForecast(lon, lat);
+            })
+        }
+    })
+}
+
+var getForecast = function (lon, lat) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly,alerts&appid=" + apiKey
+
+    fetch(apiUrl).then(function (response){
+        if (response.ok) {
+            response.json().then(function (data){
                 console.log(data);
+                for (var i = 0; i < 5; i++){
+                    // console.log(Math.floor(data.daily[i].temp.temp));
+                    console.log(Math.floor(data.daily[i].temp.min));
+                    console.log(Math.floor(data.daily[i].temp.max));
+                    console.log(data.daily[i].weather[0].main)
+                    console.log("---------------------------------------------")
+                }
             })
         }
     })
