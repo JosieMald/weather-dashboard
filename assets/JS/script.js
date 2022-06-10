47d24ae78a1ce01c437c0b776751efb7390d68d6
Lucre <josielindamaldonado@gmail.com>
2022-06-10 14:40:00 -0400

    consolidated api call to one to fetch current weather and daily weather data.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,54 +1,73 @@
-var apiKey = '2524e9bb2b5f261c8205802844842a68';
+var apiKey = "2524e9bb2b5f261c8205802844842a68";
 
 // FUNCTION CALLS ---------------------------------------------------------------------------
 var getGeocoding = function () {
-    var zipcode = '30052';
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=" + apiKey;
-    
-    fetch(apiUrl).then(function (response){
-        if (response.ok) {
-            response.json().then(function (data){
-                var lon = data.coord.lon;
-                var lat = data.coord.lat;
-                // console.log(data);
-                getCurrentWeather(lon, lat);
-            })
-        }
-    })
+  var zipcode = "30052";
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?zip=" +
+    zipcode +
+    ",us&appid=" +
+    apiKey;
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        var lon = data.coord.lon;
+        var lat = data.coord.lat;
+        var city = data.name;
+        getForecast(lon, lat, city);
+      });
+    }
+  });
 };
 getGeocoding();
 
-var getCurrentWeather = function (lon, lat) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey
+var getForecast = function (lon, lat, city) {
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&units=imperial&exclude=minutely,hourly,alerts&appid=" +
+    apiKey;
 
-    fetch(apiUrl).then(function (response){
-        if (response.ok) {
-            response.json().then(function (data){
-                // console.log(data);
-                // console.log(data.name);
-                // console.log(Math.floor(data.main.temp));
-                // console.log(data.weather[0].main)
-                getForecast(lon, lat);
-            })
-        }
-    })
-}
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        var current = data.current;
+        var daily = data.daily;
+        displayCurrentWeather(current, daily, city);
+        // console.log(data.name);
+        // console.log(Math.floor(data.main.temp));
+        // console.log(data.weather[0].main)
+      });
+    }
+  });
+};
 
-var getForecast = function (lon, lat) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly,alerts&appid=" + apiKey
+var displayCurrentWeather = function (current, daily, city) {
+    var currentTemp = Math.floor(current.temp)
+  console.log(city);
+  console.log(currentTemp);
+  console.log(current.weather[0].description);
+};
 
-    fetch(apiUrl).then(function (response){
-        if (response.ok) {
-            response.json().then(function (data){
-                console.log(data);
-                for (var i = 0; i < 5; i++){
-                    // console.log(Math.floor(data.daily[i].temp.temp));
-                    console.log(Math.floor(data.daily[i].temp.min));
-                    console.log(Math.floor(data.daily[i].temp.max));
-                    console.log(data.daily[i].weather[0].main)
-                    console.log("---------------------------------------------")
-                }
-            })
-        }
-    })
-}
+// var getForecast = function (lon, lat) {
+//     // use api url below to make one api call for the current weather and forecast.
+// var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly,alerts&appid=" + apiKey
+
+//     fetch(apiUrl).then(function (response){
+//         if (response.ok) {
+//             response.json().then(function (data){
+//                 console.log(data);
+//                 for (var i = 0; i < 5; i++){
+//                     // console.log(Math.floor(data.daily[i].temp.temp));
+//                     console.log(Math.floor(data.daily[i].temp.min));
+//                     console.log(Math.floor(data.daily[i].temp.max));
+//                     console.log(data.daily[i].weather[0].main)
+//                     console.log("---------------------------------------------")
+//                 }
+//             })
+//         }
+//     })
+// }
